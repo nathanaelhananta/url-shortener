29bb2b8b289ccd107e000748cdac94bcca28067b
Nathanael Hananta Jevano <88374559+ZeroChanBot@users.noreply.github.com>
2021-12-16 15:37:42 +0000

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,9 +1,9 @@
 const monk = require('monk')
 
 // Connection URL
-var url = "";
+var url = "MONGODB=mongodb+srv://nathanael:claramylove@cluster0.eqj2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 try {
-    if (url == '') throw console.log('Cek konfigurasi database, var url belum diisi');
+    if (url == 'MONGODB=mongodb+srv://nathanael:claramylove@cluster0.eqj2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority') throw console.log('Cek konfigurasi database, var url belum diisi');
 } catch (e) {
     return;
 }
